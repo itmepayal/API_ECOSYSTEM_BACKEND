f0fccc4edee8471241c7d8f41179bbea02efc5e5
tejash <tejashyadavinfo@gmail.com>
2026-03-11 13:43:38 +0530

    setup production settings and deployment config

--- config/settings.py
+++ config/settings.py
@@ -64,6 +64,9 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     "corsheaders.middleware.CorsMiddleware",
     "django.middleware.security.SecurityMiddleware",
+
+    "whitenoise.middleware.WhiteNoiseMiddleware",
+
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
@@ -154,8 +157,10 @@ USE_TZ = True
 # -------------------------------------------------
 # Static Files
 # -------------------------------------------------
-STATIC_URL = "static/"
+STATIC_URL = "/static/"
 STATIC_ROOT = BASE_DIR / "staticfiles"
+
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 # -------------------------------------------------
 # Custom User Model
