489c9fe7a8f1703ec91c7fca2ab5a50953a59cc9
tejash <tejashyadavinfo@gmail.com>
2026-03-11 14:14:05 +0530

    fix allowed hosts error

--- config/settings.py
+++ config/settings.py
@@ -31,12 +31,7 @@ SECRET_KEY = os.getenv("SECRET_KEY", "unsafe-secret-key")
 
 DEBUG = os.getenv("DEBUG", "True") == "True"
 
-ALLOWED_HOSTS = os.getenv(
-    "ALLOWED_HOSTS",
-    "127.0.0.1,localhost,.vercel.app",
-    "api-ecosystem.up.railway.app",
-).split(",")
-
+ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS", "").split(",")
 # -------------------------------------------------
 # Applications
 # -------------------------------------------------
