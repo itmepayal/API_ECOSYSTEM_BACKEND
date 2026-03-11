b69d1670288b3942871218aa4d32df05614a9020
tejash <tejashyadavinfo@gmail.com>
2026-03-11 14:04:11 +0530

    fix swagger routes

--- config/urls.py
+++ config/urls.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 from django.urls import path, include
 
+from rest_framework.permissions import AllowAny
+
 from drf_spectacular.views import (
     SpectacularAPIView,
     SpectacularSwaggerView,
@@ -10,11 +12,29 @@ from drf_spectacular.views import (
 urlpatterns = [
     path("admin/", admin.site.urls),
 
-    path("api/v1/accounts/", include(("accounts.urls", "accounts"), namespace="accounts")),
+    path(
+        "api/v1/accounts/",
+        include(("accounts.urls", "accounts"), namespace="accounts")
+    ),
 
-    path("api/v1/schema/", SpectacularAPIView.as_view(), name="schema"),
+    # API Schema
+    path(
+        "api/v1/schema/",
+        SpectacularAPIView.as_view(permission_classes=[AllowAny]),
+        name="schema",
+    ),
 
-    path("swagger/", SpectacularSwaggerView.as_view(url_name="schema"), name="swagger-ui"),
+    # Swagger UI
+    path(
+        "swagger/",
+        SpectacularSwaggerView.as_view(url_name="schema", permission_classes=[AllowAny]),
+        name="swagger-ui",
+    ),
 
-    path("redoc/", SpectacularRedocView.as_view(url_name="schema"), name="redoc"),
+    # ReDoc UI
+    path(
+        "redoc/",
+        SpectacularRedocView.as_view(url_name="schema", permission_classes=[AllowAny]),
+        name="redoc",
+    ),
 ]
